0e9c193176fdd63e605e89c82918dac67be98cd9
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2022-11-24 15:54:18 +0800

    二叉树前序、中序、后序遍历

---
+++ src/main/java/com/serendipity/binarytree/BinaryTree.java
@@ -0,0 +1,87 @@
+package com.serendipity.binarytree;
+
+public class BinaryTree {
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+
+        head.left.left.left = new Node(8);
+        head.left.left.right = new Node(9);
+        head.left.right.left = new Node(10);
+        head.left.right.right = new Node(11);
+        head.right.left.left = new Node(12);
+        head.right.left.right = new Node(13);
+        head.right.right.left = new Node(14);
+        head.right.right.right = new Node(15);
+        preorderTraversal(head);
+        System.out.println("=======================");
+        inorderTraversal(head);
+        System.out.println("=======================");
+        postOrderTraversal(head);
+        System.out.println("=======================");
+    }
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    // 先序打印所有节点，前序遍历      头左右
+    // 中序打印所有节点，             左头右
+    // 后序打印所有节点，前序遍历      左右头
+
+    public static void traversal(Node head) {
+        if (head == null) {
+            return;
+        }
+        // 前序
+        System.out.println(head.val);
+        traversal(head.left);
+        // 中序
+        // System.out.println(head.val);
+        traversal(head.right);
+        // 后序
+        // System.out.println(head.val);
+    }
+
+    public static void preorderTraversal(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.val);
+        System.out.print(" ");
+        preorderTraversal(head.left);
+        preorderTraversal(head.right);
+    }
+
+    public static void inorderTraversal(Node head) {
+        if (head == null) {
+            return;
+        }
+        inorderTraversal(head.left);
+        System.out.print(head.val);
+        System.out.print(" ");
+        inorderTraversal(head.right);
+    }
+
+    public static void postOrderTraversal(Node head) {
+        if (head == null) {
+            return;
+        }
+        postOrderTraversal(head.left);
+        postOrderTraversal(head.right);
+        System.out.print(head.val);
+        System.out.print(" ");
+    }
+
+}
