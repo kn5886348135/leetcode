51b280d2dbe2f1c8f15679572c8df230fde3d1db
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2022-11-25 16:28:21 +0800

    选择、冒泡、插入排序

--- src/main/java/com/serendipity/sort/Sort.java
+++ src/main/java/com/serendipity/sort/Sort.java
@@ -1,7 +1,5 @@
 package com.serendipity.sort;
 
-import java.util.Arrays;
-
 /**
  * 排序算法
  */
@@ -10,29 +8,26 @@ public class Sort {
         int maxLen = 50;
         int maxValue = 3000;
         int times = 10000;
-        int[] arr = randomArr(maxLen, maxValue);
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ",");
-        }
-        System.out.println();
         for (int i = 0; i < times; i++) {
-            arr = randomArr(maxLen, maxValue);
+            int[] arr = randomArr(maxLen, maxValue);
             int[] tmp = copyArr(arr);
-            //        selectionSort(arr);
-            //        bubbleSort(arr);
+            // selectionSort(arr);
+            // bubbleSort(arr);
             insertSort(arr);
             boolean result = validateArr(arr);
             if (!result) {
-                for (int j = 0; j < arr.length; j++) {
+                for (int j = 0; j < tmp.length; j++) {
                     System.out.print(tmp[j] + " ");
+                }
+                System.out.println();
+                for (int j = 0; j < arr.length; j++) {
+                    System.out.print(arr[j] + " ");
                 }
                 System.out.println();
                 System.out.println("排序结果错误");
                 break;
             }
         }
-
     }
     
     private static int[] copyArr(int[] arr){
@@ -44,13 +39,15 @@ public class Sort {
     }
 
     private static boolean validateArr(int[] arr) {
-        if (arr.length < 2) {
+        if ((arr == null || arr.length < 1)) {
             return true;
         }
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
             if (max > arr[i]) {
                 return false;
+            } else {
+                max = arr[i];
             }
         }
         return true;
@@ -68,63 +65,58 @@ public class Sort {
         return arr;
     }
 
+    // 选择一个最小值，交换到有序部分的末位置，不稳定
     private static void selectionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-
         int length = arr.length;
-        int tmp;
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length - 1; i++) {
             int index = i;
-            for (int j = i; j < length; j++) {
+            for (int j = i + 1; j < length; j++) {
                 // 小于号避免位置交换
                 index = arr[j] < arr[index] ? j : index;
             }
-            if (index == i) {
-                continue;
-            }
-            tmp = arr[i];
-            arr[i] = arr[index];
-            arr[index] = tmp;
+            swap(arr, i, index);
         }
     }
 
+    // 相邻两个数据，两两交换
     private static void bubbleSort(int[] arr){
         int length = arr.length;
         if (arr == null || length < 2) {
             return;
         }
 
-        int tmp;
         for (int i = length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j + 1] < arr[j]) {
-                    tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
+                    swap(arr, j, j + 1);
                 }
             }
         }
     }
 
+    // 前半部分有序，每次拿后半部分第一个数据依次交换到正确位置。
     private static void insertSort(int[] arr){
         int length = arr.length;
         if (arr == null || length < 2) {
             return;
         }
-        int tmp;
-        for (int i = 0; i < length; i++) {
-            for (int j = i; j > 0; j--) {
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
                 if (arr[j - 1] > arr[j]) {
-                    tmp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = tmp;
+                    swap(arr, j - 1, j);
                 } else {
                     break;
                 }
-
             }
         }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
