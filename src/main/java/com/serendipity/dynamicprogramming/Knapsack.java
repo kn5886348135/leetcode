7c23eecaef35f577d63dc20694d20dc7f7fd3259
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2022-12-20 15:50:55 +0800

    动态规划 背包问题

--- src/main/java/com/serendipity/dynamicprogramming/Knapsack.java
+++ src/main/java/com/serendipity/dynamicprogramming/Knapsack.java
@@ -10,37 +10,40 @@ public class Knapsack {
 
     /**
      * 为了方便没有负数，负数也是可以满足的
+     *
      * @param w 重量数组
      * @param v 价值数组
      * @param bag 背包容量，不能超过这个载重
-     * @return
+     * @return 不超重的情况下，能够得到的最大价值
      */
     private static int maxValue(int[] w, int[] v, int bag) {
         if (w == null || v == null || w.length != v.length || w.length == 0) {
             return 0;
         }
+        // 尝试函数！
         return process(w, v, 0, bag);
     }
 
-    private static int process(int[] w, int[] v, int index, int bag) {
-        if (bag < 0) {
+    // index 0~N
+    // rest 负~bag
+    private static int process(int[] w, int[] v, int index, int rest) {
+        if (rest < 0) {
             return -1;
         }
         if (index == w.length) {
             return 0;
         }
 
-        int p1 = process(w, v, index + 1, bag);
+        int p1 = process(w, v, index + 1, rest);
         int p2 = 0;
-        int next =  process(w, v, index + 1, bag - w[index]);
+        int next = process(w, v, index + 1, rest - w[index]);
         if (next != -1) {
             p2 = v[index] + next;
         }
-
         return Math.max(p1, p2);
     }
 
-    private static int maxValuedp(int[] w, int[] v, int bag) {
+    private static int dp(int[] w, int[] v, int bag) {
         if (w == null || v == null || w.length != v.length || w.length == 0) {
             return 0;
         }
@@ -58,6 +61,14 @@ public class Knapsack {
             }
         }
         return dp[0][bag];
+    }
+
+    public static void main(String[] args) {
+        int[] weights = { 3, 2, 4, 7, 3, 1, 7 };
+        int[] values = { 5, 6, 3, 19, 12, 4, 2 };
+        int bag = 15;
+        System.out.println(maxValue(weights, values, bag));
+        System.out.println(dp(weights, values, bag));
     }
 
 }
