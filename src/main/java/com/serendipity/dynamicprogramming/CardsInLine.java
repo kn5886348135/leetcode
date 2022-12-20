a92dd95dbbfe6f9b5d2dc45e080cd360fbd4fcb3
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2022-12-20 15:41:02 +0800

    添加注释

--- src/main/java/com/serendipity/dynamicprogramming/CardsInLine.java
+++ src/main/java/com/serendipity/dynamicprogramming/CardsInLine.java
@@ -3,43 +3,48 @@ package com.serendipity.dynamicprogramming;
 /**
  * @author jack
  * @version 1.0
- * @description
+ * @description 给定一个整型数组arr，代表数值不同的纸牌排成一条线。玩家A和玩家B依次拿走每张纸牌，规定玩家A
+ *              先拿，玩家B后拿。但是每个玩家每次只能拿走最左或最右的纸牌，玩家A和玩家B都绝顶聪明。请返回
+ *              最后获胜者的分数。
  * @date 2022/12/20/11:17
  */
 public class CardsInLine {
 
     private static int win1(int[] arr) {
-        if (arr == null || arr.length == 1) {
+        if (arr == null || arr.length == 0) {
             return 0;
         }
 
-        int first = f(arr, 0, arr.length - 1);
-        int second = g(arr, 0, arr.length - 1);
+        int first = sente1(arr, 0, arr.length - 1);
+        int second = gote1(arr, 0, arr.length - 1);
         return Math.max(first, second);
     }
 
-    private static int f(int[] arr, int left, int right) {
+    // arr[left..right]，先手获得的最好分数返回
+    private static int sente1(int[] arr, int left, int right) {
         if (left == right) {
             return arr[left];
         }
-        int p1 = arr[left] + g(arr, left + 1, right);
-        int p2 = arr[right] + g(arr, left, right - 1);
+        int p1 = arr[left] + gote1(arr, left + 1, right);
+        int p2 = arr[right] + gote1(arr, left, right - 1);
         return Math.max(p1, p2);
     }
 
-    private static int g(int[] arr, int left, int right) {
+    // arr[left..right]，后手获得的最好分数返回
+    private static int gote1(int[] arr, int left, int right) {
         if (left == right) {
             return 0;
         }
-
-        int p1 = f(arr, left + 1, right);
-        int p2 = f(arr, left, right - 1);
+        // 对手拿走了left位置的数
+        int p1 = sente1(arr, left + 1, right);
+        // 对手拿走了right位置的数
+        int p2 = sente1(arr, left, right - 1);
         return Math.min(p1, p2);
     }
 
 
     private static int win2(int[] arr) {
-        if (arr == null || arr.length == 1) {
+        if (arr == null || arr.length == 0) {
             return 0;
         }
         int length = arr.length;
@@ -51,12 +56,13 @@ public class CardsInLine {
                 gmap[i][j] = -1;
             }
         }
-        int first = f2(arr, 0, arr.length - 1, fmap, gmap);
-        int second = g2(arr, 0, arr.length - 1, fmap, gmap);
+        int first = sente2(arr, 0, arr.length - 1, fmap, gmap);
+        int second = gote2(arr, 0, arr.length - 1, fmap, gmap);
         return Math.max(first, second);
     }
 
-    private static int f2(int[] arr, int left, int right, int[][] fmap, int[][] gmap) {
+    // arr[left..right]，先手获得的最好分数返回
+    private static int sente2(int[] arr, int left, int right, int[][] fmap, int[][] gmap) {
         if (fmap[left][right] != -1) {
             return fmap[left][right];
         }
@@ -64,22 +70,25 @@ public class CardsInLine {
         if (left == right) {
             ans = arr[left];
         } else {
-            int p1 = arr[left] + g2(arr, left + 1, right, fmap, gmap);
-            int p2 = arr[right] + g2(arr, left, right - 1, fmap, gmap);
+            int p1 = arr[left] + gote2(arr, left + 1, right, fmap, gmap);
+            int p2 = arr[right] + gote2(arr, left, right - 1, fmap, gmap);
             ans = Math.max(p1, p2);
         }
         fmap[left][right] = ans;
         return ans;
     }
 
-    private static int g2(int[] arr, int left, int right,int[][] fmap, int[][] gmap) {
+    // // arr[left..right]，后手获得的最好分数返回
+    private static int gote2(int[] arr, int left, int right, int[][] fmap, int[][] gmap) {
         if (gmap[left][right] != -1) {
             return gmap[left][right];
         }
         int ans = 0;
         if (left != right) {
-            int p1 = f2(arr, left + 1, right, fmap, gmap);
-            int p2 = f2(arr, left, right - 1, fmap, gmap);
+            // 对手拿走了left位置的数
+            int p1 = sente2(arr, left + 1, right, fmap, gmap);
+            // 对手拿走了right位置的数
+            int p2 = sente2(arr, left, right - 1, fmap, gmap);
             ans = Math.min(p1, p2);
         }
         gmap[left][right] = ans;
@@ -88,7 +97,7 @@ public class CardsInLine {
 
 
     private static int win3(int[] arr) {
-        if (arr == null || arr.length == 1) {
+        if (arr == null || arr.length == 0) {
             return 0;
         }
         int length = arr.length;
@@ -103,7 +112,7 @@ public class CardsInLine {
             int right = startcol;
             while (right < length) {
                 fmap[left][right] = Math.max(arr[left] + gmap[left + 1][right], arr[right] + gmap[left][right - 1]);
-                fmap[left][right] = Math.min(fmap[left + 1][right], fmap[left][right - 1]);
+                gmap[left][right] = Math.min(fmap[left + 1][right], fmap[left][right - 1]);
                 left++;
                 right++;
             }
@@ -118,5 +127,4 @@ public class CardsInLine {
         System.out.println(win3(arr));
 
     }
-
 }
--- src/main/java/com/serendipity/dynamicprogramming/RobotWalk.java
+++ src/main/java/com/serendipity/dynamicprogramming/RobotWalk.java
@@ -3,16 +3,20 @@ package com.serendipity.dynamicprogramming;
 /**
  * @author jack
  * @version 1.0
- * @description
+ * @description 假设有排成一行的N个位置，记为1~N，N 一定大于或等于2。开始时机器人在其中的M位置上(M一定是 1~N 中的一个)，如果
+ *              机器人来到1位置，那么下一步只能往右来到2位置；如果机器人来到N位置，那么下一步只能往左来到 N-1 位置；如果机器人
+ *              来到中间位置，那么下一步可以往左走或者往右走；规定机器人必须走 K 步，最终能来到P位置(P也是1~N中的一个)的方法有
+ *              多少种？
+ *              给定四个参数 N、M、K、P，返回方法数。
  * @date 2022/12/20/10:31
  */
 public class RobotWalk {
 
-    private static int ways1(int n,int start,int aim,int k) {
+    private static int ways1(int n, int start, int aim, int k) {
         if (n < 2 || start < 1 || start > n || aim < 1 || aim > n || k < 1) {
             return -1;
         }
-        return processes1(start, k, aim, n);
+        return process1(start, k, aim, n);
     }
 
     /**
@@ -22,21 +26,23 @@ public class RobotWalk {
      * @param n    有哪些位置？1-N
      * @return 机器人从cur出发走过rest步之后最终停在aim的方法数
      */
-    private static int processes1(int cur, int rest, int aim, int n) {
+    private static int process1(int cur, int rest, int aim, int n) {
         // 递归终止条件
         if (rest == 0) {
             return cur == aim ? 1 : 0;
         }
+        // 当前在最左边，只能向右
         if (cur == 1) {
-            return processes1(2, rest - 1, aim, n);
+            return process1(2, rest - 1, aim, n);
         }
+        // 当前在最右边，只能向左
         if (cur == n) {
-            return processes1(n - 1, rest - 1, aim, n);
+            return process1(n - 1, rest - 1, aim, n);
         }
-        return processes1(cur - 1, rest - 1, aim, n) + processes1(cur + 1, rest - 1, aim, n);
+        return process1(cur - 1, rest - 1, aim, n) + process1(cur + 1, rest - 1, aim, n);
     }
 
-    private static int ways2(int n,int start,int aim,int k) {
+    private static int ways2(int n, int start, int aim, int k) {
         if (n < 2 || start < 1 || start > n || aim < 1 || aim > n || k < 1) {
             return -1;
         }
@@ -47,10 +53,16 @@ public class RobotWalk {
                 dp[i][j] = -1;
             }
         }
-        return processes2(start, k, aim, n, dp);
+        // dp就是缓存表
+        // dp[cur][rest] == -1 -> process1(cur, rest)之前没算过！
+        // dp[cur][rest] != -1 -> process1(cur, rest)之前算过！返回值，dp[cur][rest]
+        // N+1 * K+1
+        return process2(start, k, aim, n, dp);
     }
 
-    private static int processes2(int cur, int rest, int aim, int n,int[][] dp) {
+    // cur 范: 1 ~ N
+    // rest 范：0 ~ K
+    private static int process2(int cur, int rest, int aim, int n, int[][] dp) {
         if (dp[cur][rest] != -1) {
             return dp[cur][rest];
         }
@@ -60,18 +72,18 @@ public class RobotWalk {
         if (rest == 0) {
             ans = cur == aim ? 1 : 0;
         } else if (cur == 1) {
-            ans = processes2(2, rest - 1, aim, n, dp);
+            ans = process2(2, rest - 1, aim, n, dp);
         } else if (cur == n) {
-            ans = processes2(n - 1, rest - 1, aim, n, dp);
-        }else {
-            ans = processes2(cur - 1, rest - 1, aim, n, dp) + processes2(cur + 1, rest - 1, aim, n, dp);
+            ans = process2(n - 1, rest - 1, aim, n, dp);
+        } else {
+            ans = process2(cur - 1, rest - 1, aim, n, dp) + process2(cur + 1, rest - 1, aim, n, dp);
         }
         dp[cur][rest] = ans;
         return ans;
     }
 
     // 根据递归画出图形，拿到状态转移方程
-    private static int ways3(int n,int start,int aim,int k) {
+    private static int ways3(int n, int start, int aim, int k) {
         if (n < 2 || start < 1 || start > n || aim < 1 || aim > n || k < 1) {
             return -1;
         }
@@ -87,4 +99,9 @@ public class RobotWalk {
         return dp[start][k];
     }
 
+    public static void main(String[] args) {
+        System.out.println(ways1(5, 2, 4, 6));
+        System.out.println(ways2(5, 2, 4, 6));
+        System.out.println(ways3(5, 2, 4, 6));
+    }
 }
