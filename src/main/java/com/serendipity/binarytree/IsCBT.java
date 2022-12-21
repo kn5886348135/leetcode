bcf67d8ad21bef28657909d6249ec1163de509d3
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2022-12-21 17:18:54 +0800

    二叉树的递归套路 是否是完全二叉树

---
+++ src/main/java/com/serendipity/binarytree/IsCBT.java
@@ -0,0 +1,124 @@
+package com.serendipity.binarytree;
+
+
+import java.util.LinkedList;
+
+/**
+ * @author jack
+ * @version 1.0
+ * @description 判断一棵二叉树是否为完全二叉树
+ *              按照顺序，完全二叉树和满二叉树重合，完全二叉树只有叶子节点的右边不满。
+ * @date 2022/12/21/16:18
+ */
+public class IsCBT {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static boolean isCBT1(Node head) {
+        if (head == null) {
+            return true;
+        }
+
+        LinkedList<Node> queue = new LinkedList<>();
+        // 是否叶子节点
+        boolean leaf = false;
+        Node left;
+        Node right;
+        queue.add(head);
+        while (!queue.isEmpty()) {
+            head = queue.poll();
+            left = head.left;
+            right = head.right;
+            // leaf为true表示二叉树当前行前面出现叶子节点，此时后续节点的子节点应该都为空
+            // 完全二叉树中，左节点为空，右节点必须为空
+            if ((leaf && (left != null || right != null)) || (left == null && right != null)) {
+                return false;
+            }
+            if (left != null) {
+                queue.add(left);
+            }
+            if (right != null) {
+                queue.add(right);
+            }
+            // 判断叶子节点
+            if (left == null || right == null) {
+                leaf = true;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isCBT2(Node head) {
+        return process(head).isCBT;
+    }
+
+    public static class Info {
+        public boolean isFull;
+        public boolean isCBT;
+        public int height;
+
+        public Info(boolean isFull, boolean isCBT, int height) {
+            this.isFull = isFull;
+            this.isCBT = isCBT;
+            this.height = height;
+        }
+    }
+
+    // 后序遍历
+    public static Info process(Node head) {
+        if (head == null) {
+            return new Info(true, true, 0);
+        }
+        Info leftInfo = process(head.left);
+        Info rightInfo = process(head.right);
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        boolean isFull = leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height;
+        boolean isCBT = false;
+        // 满二叉树是完全二叉树
+        if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height) {
+            isCBT = true;
+        } else if (leftInfo.isCBT && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
+            isCBT = true;
+        } else if (leftInfo.isFull && rightInfo.isFull && leftInfo.height == rightInfo.height + 1) {
+            isCBT = true;
+        } else if (leftInfo.isFull && rightInfo.isCBT && leftInfo.height == rightInfo.height) {
+            isCBT = true;
+        }
+        return new Info(isFull, isCBT, height);
+    }
+
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 5;
+        int maxValue = 100;
+        int count = 1000000;
+        for (int i = 0; i < count; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (isCBT1(head) != isCBT2(head)) {
+                System.out.println("failed");
+            }
+        }
+        System.out.println("success");
+    }
+}
