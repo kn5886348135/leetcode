b6fb99d93a29a974200d7cb66c8502d077007ec9
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2023-03-17 18:03:35 +0800

    Update ZeroLeftOneStringNumber.java
    2 * n长度使用1 * 2铺满瓷砖，有多少方法

--- src/main/java/com/serendipity/fibonacci/ZeroLeftOneStringNumber.java
+++ src/main/java/com/serendipity/fibonacci/ZeroLeftOneStringNumber.java
@@ -11,25 +11,74 @@ package com.serendipity.fibonacci;
 public class ZeroLeftOneStringNumber {
 
     public static void main(String[] args) {
-
+        for (int i = 0; i != 20; i++) {
+            System.out.println(fibonacci1(i));
+            System.out.println(fibonacci2(i));
+            System.out.println(fibonacci3(i));
+            System.out.println("===================");
+        }
     }
 
     // 斐波那契问题
-    // 列出每一项，观察得出
+    // 列出每一项，观察得出结论
     // 按照第一位是0或者1推理得出fn = fn-1 + fn-2
-    public static int zeroLeftOneStringNumber(int n) {
+    public static int fibonacci1(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        return process(1, n);
+    }
+
+    public static int process(int i, int n) {
+        if (i == n - 1) {
+            return 2;
+        }
+        if (i == n) {
+            return 1;
+        }
+        return process(i + 1, n) + process(i + 2, n);
+    }
+
+    public static int fibonacci2(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int pre = 1;
+        int cur = 1;
+        int tmp = 0;
+        for (int i = 2; i < n + 1; i++) {
+            tmp = cur;
+            cur += pre;
+            pre = tmp;
+        }
+        return cur;
+    }
+
+    public static int fibonacci3(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int[][] base = { { 1, 1 }, { 1, 0 } };
+        int[][] res = matrixPower(base, n - 2);
+        return 2 * res[0][0] + res[1][0];
+    }
+
+    // 2 * n 长度铺满 1 * 2的瓷砖有多少种方式
+    public static int fi(int n) {
         if (n < 1) {
             return 0;
         }
         if (n == 1 || n == 2) {
             return 1;
         }
-        // [ 1 ,1 ]
-        // [ 1, 0 ]
-        int[][] base = {
-                { 1, 1 },
-                { 1, 0 }
-        };
+        int[][] base = { { 1, 1 },
+                { 1, 0 } };
         int[][] res = matrixPower(base, n - 2);
         return res[0][0] + res[1][0];
     }
@@ -39,14 +88,12 @@ public class ZeroLeftOneStringNumber {
         for (int i = 0; i < res.length; i++) {
             res[i][i] = 1;
         }
-        // res = 矩阵中的1
-        int[][] t = m;
-        // 矩阵1次方
+        int[][] tmp = m;
         for (; p != 0; p >>= 1) {
             if ((p & 1) != 0) {
-                res = product(res, t);
+                res = product(res, tmp);
             }
-            t = product(t, t);
+            tmp = product(tmp, tmp);
         }
         return res;
     }
@@ -55,18 +102,16 @@ public class ZeroLeftOneStringNumber {
     public static int[][] product(int[][] a, int[][] b) {
         int n = a.length;
         int m = b[0].length;
-        int k = a[0].length;
-        // a的列数同时也是b的行数
+        int k = a[0].length; // a的列数同时也是b的行数
         int[][] ans = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                for (int c = 0; c < k; c++) {
+        for(int i = 0 ; i < n; i++) {
+            for(int j = 0 ; j < m;j++) {
+                for(int c = 0; c < k; c++) {
                     ans[i][j] += a[i][c] * b[c][j];
                 }
             }
         }
         return ans;
     }
-
 
 }
