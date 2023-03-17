d809a36a9add60ec83f473b287c6cff98b78d232
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2023-03-17 17:45:15 +0800

    Update FibonacciProblem.java
    斐波那契问题

--- src/main/java/com/serendipity/fibonacci/FibonacciProblem.java
+++ src/main/java/com/serendipity/fibonacci/FibonacciProblem.java
@@ -9,6 +9,21 @@ package com.serendipity.fibonacci;
 public class FibonacciProblem {
 
     public static void main(String[] args) {
+        int n = 19;
+        System.out.println(fibonacci1(n));
+        System.out.println(fibonacci2(n));
+        System.out.println(fibonacci3(n));
+        System.out.println("===");
+
+        System.out.println(s1(n));
+        System.out.println(s2(n));
+        System.out.println(s3(n));
+        System.out.println("===");
+
+        System.out.println(c1(n));
+        System.out.println(c2(n));
+        System.out.println(c3(n));
+        System.out.println("===");
 
     }
 
@@ -32,15 +47,15 @@ public class FibonacciProblem {
             return 1;
         }
 
-        int first = 1;
-        int second = 1;
+        int res = 1;
+        int pre = 1;
         int tmp = 0;
         for (int i = 3; i <= n; i++) {
-            tmp = first;
-            first = first + second;
-            second = tmp;
+            tmp = res;
+            res = res + pre;
+            pre = tmp;
         }
-        return first;
+        return res;
     }
 
     // O(logn)
@@ -61,7 +76,6 @@ public class FibonacciProblem {
               };
         int[][] res = matrixPower(base, n - 2);
         return res[0][0] + res[1][0];
-
     }
 
     public static int[][] matrixPower(int[][] m, int p) {
@@ -98,5 +112,98 @@ public class FibonacciProblem {
         return ans;
     }
 
+    // 一个人可以一次往上迈1个台阶，也可以迈2个台阶
+    //返回这个人迈上N级台阶的方法数
+    public static int s1(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        return s1(n - 1) + s1(n - 2);
+    }
+
+    public static int s2(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int res = 2;
+        int pre = 1;
+        int tmp = 0;
+        for (int i = 3; i <= n; i++) {
+            tmp = res;
+            res = res + pre;
+            pre = tmp;
+        }
+        return res;
+    }
+
+    public static int s3(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int[][] base = { { 1, 1 }, { 1, 0 } };
+        int[][] res = matrixPower(base, n - 2);
+        return 2 * res[0][0] + res[1][0];
+    }
+
+    // 第一年农场有1只成熟的母牛A，往后的每年：
+    //1）每一只成熟的母牛都会生一只母牛
+    //2）每一只新出生的母牛都在出生的第三年成熟
+    //3）每一只母牛永远不会死
+    //返回N年后牛的数量
+    public static int c1(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2 || n == 3) {
+            return n;
+        }
+        return c1(n - 1) + c1(n - 3);
+    }
+
+    public static int c2(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2 || n == 3) {
+            return n;
+        }
+        int res = 3;
+        int pre = 2;
+        int prepre = 1;
+        int tmp1 = 0;
+        int tmp2 = 0;
+        for (int i = 4; i <= n; i++) {
+            tmp1 = res;
+            tmp2 = pre;
+            res = res + prepre;
+            pre = tmp1;
+            prepre = tmp2;
+        }
+        return res;
+    }
+
+    public static int c3(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2 || n == 3) {
+            return n;
+        }
+        int[][] base = {
+                { 1, 1, 0 },
+                { 0, 0, 1 },
+                { 1, 0, 0 } };
+        int[][] res = matrixPower(base, n - 3);
+        return 3 * res[0][0] + 2 * res[1][0] + res[2][0];
+    }
 
 }
