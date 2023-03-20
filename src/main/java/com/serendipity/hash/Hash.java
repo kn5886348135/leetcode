60dcd19623e9f85ed11fc982117125ebdcd671fa
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2023-03-20 16:52:32 +0800

    hash

---
+++ src/main/java/com/serendipity/hash/Hash.java
@@ -0,0 +1,51 @@
+package com.serendipity.hash;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+
+/**
+ * @author jack
+ * @version 1.0
+ * @description
+ * @date 2023/03/20/16:48
+ */
+public class Hash {
+
+    public static void main(String[] args) {
+        System.out.println("支持的算法 : ");
+        for (String str : Security.getAlgorithms("MessageDigest")) {
+            System.out.println(str);
+        }
+        System.out.println("=======");
+
+        String algorithm = "MD5";
+        Hash hash = new Hash(algorithm);
+
+        String input1 = "guanyunchangqianlizoudanqi1";
+        String input2 = "guanyunchangqianlizoudanqi2";
+        String input3 = "guanyunchangqianlizoudanqi3";
+        String input4 = "guanyunchangqianlizoudanqi4";
+        String input5 = "guanyunchangqianlizoudanqi5";
+        System.out.println(hash.hashCode(input1));
+        System.out.println(hash.hashCode(input2));
+        System.out.println(hash.hashCode(input3));
+        System.out.println(hash.hashCode(input4));
+        System.out.println(hash.hashCode(input5));
+    }
+
+    private MessageDigest hash;
+
+    public Hash(String algorithm ) {
+        try {
+            hash = MessageDigest.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+            noSuchAlgorithmException.printStackTrace();
+        }
+    }
+
+    public String hashCode(String input) {
+        return DatatypeConverter.printHexBinary(hash.digest(input.getBytes())).toUpperCase();
+    }
+}
--- src/main/java/com/serendipity/indextree/AhoCorasick2.java
+++ src/main/java/com/serendipity/indextree/AhoCorasick2.java
@@ -1,43 +1,48 @@
 package com.serendipity.indextree;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
  * @author jack
  * @version 1.0
- * @description Aho–Corasick算法  AC自动机
- *              解决在一个大字符串中，找到多个候选字符串的问题
- *
- *              AC自动机算法核心
- *              1）把所有匹配串生成一棵前缀树
- *              2）前缀树节点增加fail指针
- *              3）fail指针的含义：如果必须以当前字符结尾，当前形成的路径是str，剩下哪一个字符串的前缀和str的后缀，拥有
- *              最大的匹配长度。fail指针就指向那个字符串的最后一个字符所对应的节点。（迷不迷？听讲述！）
- * @date 2023/03/19/21:58
+ * @description
+ * @date 2023/03/20/13:46
  */
-public class AhoCorasick {
+public class AhoCorasick2 {
 
     public static void main(String[] args) {
         AC ac = new AC();
         ac.insert("dhe");
         ac.insert("he");
-        ac.insert("c");
+        ac.insert("abcdheks");
+        // 设置fail指针
         ac.build();
-        System.out.println(ac.containNum("cdhe"));
+
+        List<String> contains = ac.containWords("abcdhekskdjfafhasldkflskdjhwqaeruv");
+        for (String word : contains) {
+            System.out.println(word);
+        }
     }
 
+    // 前缀树的节点
     public static class Node {
-        // 有多少个字符串以该节点结尾
-        public int end;
+        // 如果一个node，end为空，不是结尾
+        // 如果end不为空，表示这个点是某个字符串的结尾，end的值就是这个字符串
+        public String end;
+        // 只有在上面的end变量不为空的时候，endUse才有意义
+        // 表示，这个字符串之前有没有加入过答案
+        public boolean endUse;
         public Node fail;
         public Node[] nexts;
 
         public Node() {
-            this.end = 0;
-            this.fail = null;
-            // 因为英文只有26个字母
-            this.nexts = new Node[26];
+            endUse = false;
+            end = null;
+            fail = null;
+            nexts = new Node[26];
         }
     }
 
@@ -45,10 +50,9 @@ public class AhoCorasick {
         private Node root;
 
         public AC() {
-            this.root = new Node();
+            root = new Node();
         }
 
-        // 调用insert，加入匹配串
         public void insert(String str) {
             char[] chs = str.toCharArray();
             Node cur = root;
@@ -60,27 +64,22 @@ public class AhoCorasick {
                 }
                 cur = cur.nexts[index];
             }
-            cur.end++;
+            cur.end = str;
         }
 
-        // 构建fail指针
         public void build() {
-            // 宽度优先搜索
             Queue<Node> queue = new LinkedList<>();
             queue.add(root);
             Node cur = null;
             Node fail = null;
             while (!queue.isEmpty()) {
-                // cur作为父节点
+                // 某个父亲，cur
                 cur = queue.poll();
-                for (int i = 0; i < 26; i++) {
-                    // 宽度优先所有遍历所有子节点
-                    // 该路下有子节点
-                    if (cur.nexts[i] != null) {
-                        // 初始时先设置一个值
+                for (int i = 0; i < 26; i++) { // 所有的路
+                    // cur -> 父亲  i号儿子，必须把i号儿子的fail指针设置好！
+                    if (cur.nexts[i] != null) { // 如果真的有i号儿子
                         cur.nexts[i].fail = root;
                         fail = cur.fail;
-                        // cur不是头节点
                         while (fail != null) {
                             if (fail.nexts[i] != null) {
                                 cur.nexts[i].fail = fail.nexts[i];
@@ -94,32 +93,40 @@ public class AhoCorasick {
             }
         }
 
-        public int containNum(String content) {
+        // 大文章：content
+        public List<String> containWords(String content) {
             char[] chs = content.toCharArray();
             Node cur = root;
             Node follow = null;
             int index = 0;
-            int ans = 0;
+            List<String> ans = new ArrayList<>();
             for (int i = 0; i < chs.length; i++) {
-                index = chs[i] - 'a';
+                index = chs[i] - 'a'; // 路
+                // 如果当前字符在这条路上没配出来，就随着fail方向走向下条路径
                 while (cur.nexts[index] == null && cur != root) {
                     cur = cur.fail;
                 }
+                // 1) 现在来到的路径，是可以继续匹配的
+                // 2) 现在来到的节点，就是前缀树的根节点
                 cur = cur.nexts[index] != null ? cur.nexts[index] : root;
                 follow = cur;
                 while (follow != root) {
-                    if (follow.end == -1) {
+                    if (follow.endUse) {
                         break;
                     }
-                    // 不同的需求，在这一段{ }之间修改
-                    {
-                        ans += follow.end;
-                        follow.end = -1;
+                    // 不同的需求，在这一段之间修改
+                    if (follow.end != null) {
+                        ans.add(follow.end);
+                        follow.endUse = true;
                     }
+                    // 不同的需求，在这一段之间修改
                     follow = follow.fail;
                 }
             }
             return ans;
         }
+
     }
+
+
 }
