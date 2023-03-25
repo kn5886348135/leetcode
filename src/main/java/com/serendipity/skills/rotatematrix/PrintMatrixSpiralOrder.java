9ce5095a60e54289a0d1e0c33fb9c484afd37ed0
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2023-03-25 22:01:34 +0800

    转圈打印矩阵

--- src/main/java/com/serendipity/skills/rotatematrix/PrintMatrixSpiralOrder.java
+++ src/main/java/com/serendipity/skills/rotatematrix/PrintMatrixSpiralOrder.java
@@ -24,7 +24,7 @@ public class PrintMatrixSpiralOrder {
     public static void spiralOrderPrint(int[][] matrix) {
         int tR = 0;
         int tC = 0;
-        int dR = matrix.length;
+        int dR = matrix.length - 1;
         int dC = matrix[0].length - 1;
         while (tR <= dR && tC <= dC) {
             printEdge(matrix, tR++, tC++, dR--, dC--);
@@ -34,29 +34,29 @@ public class PrintMatrixSpiralOrder {
     public static void printEdge(int[][] m, int tR, int tC, int dR, int dC) {
         if (tR == dR) {
             for (int i = tC; i <= dC; i++) {
-                System.out.println(m[tR][i] + " ");
+                System.out.print(m[tR][i] + " ");
             }
         } else if (tC == dC) {
             for (int i = tR; i <= dR; i++) {
-                System.out.println(m[i][tC] + " ");
+                System.out.print(m[i][tC] + " ");
             }
         } else {
             int curC = tC;
             int curR = tR;
             while (curC != dC) {
-                System.out.println(m[tR][curC] + " ");
+                System.out.print(m[tR][curC] + " ");
                 curC++;
             }
             while (curR != dR) {
-                System.out.println(m[curR][dC] + " ");
+                System.out.print(m[curR][dC] + " ");
                 curR++;
             }
             while (curC != tC) {
-                System.out.println(m[dR][curC] + " ");
+                System.out.print(m[dR][curC] + " ");
                 curC--;
             }
             while (curR != tR) {
-                System.out.println(m[curR][tC] + " ");
+                System.out.print(m[curR][tC] + " ");
                 curR--;
             }
         }
---
+++ src/main/java/com/serendipity/skills/rotatematrix/RotateMatrix.java
@@ -0,0 +1,52 @@
+package com.serendipity.skills.rotatematrix;
+
+/**
+ * @author jack
+ * @version 1.0
+ * @description 给定一个长方形矩阵matrix，实现转圈打印
+ *              a  b  c  d
+ *              e  f  g  h
+ *              i  j  k  L
+ *              打印顺序：a b c d h L k j I e f g
+ * @date 2023/03/25/21:53
+ */
+public class RotateMatrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        printMatrix(matrix);
+        rotate(matrix);
+        System.out.println("=========");
+        printMatrix(matrix);
+    }
+
+    public static void rotate(int[][] matrix) {
+        int a = 0;
+        int b = 0;
+        int c = matrix.length - 1;
+        int d = matrix[0].length - 1;
+        while (a < c) {
+            rotateEdge(matrix, a++, b++, c--, d--);
+        }
+    }
+
+    public static void rotateEdge(int[][] m, int a, int b, int c, int d) {
+        int tmp = 0;
+        for (int i = 0; i < d - b; i++) {
+            tmp = m[a][b + i];
+            m[a][b + i] = m[c - i][b];
+            m[c - i][b] = m[c][d - i];
+            m[c][d - i] = m[a + i][d];
+            m[a + i][d] = tmp;
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i != matrix.length; i++) {
+            for (int j = 0; j != matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
