31f5eca053127d3c352f1ff2eeb140d891bc9653
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2023-03-27 14:40:30 +0800

    n*m贴瓷砖

---
+++ src/main/java/com/serendipity/skills/statecompression/PavingTile.java
@@ -0,0 +1,218 @@
+package com.serendipity.skills.statecompression;
+
+/**
+ * @author jack
+ * @version 1.0
+ * @description 你有无限的1*2的砖块，要铺满M*N的区域，
+ *              不同的铺法有多少种?
+ * @date 2023/03/27/12:56
+ */
+public class PavingTile {
+
+    public static void main(String[] args) {
+        int n = 8;
+        int m = 6;
+        System.out.println(ways1(n, m));
+        System.out.println(ways2(n, m));
+        System.out.println(ways3(n, m));
+        System.out.println(ways4(n, m));
+
+        n = 10;
+        m = 10;
+        System.out.println("=========");
+        System.out.println(ways3(n, m));
+        System.out.println(ways4(n, m));
+    }
+
+    public static int ways1(int n,int m) {
+        if (n < 1 || m < 1 | ((n * m & 1) != 0)) {
+            return 0;
+        }
+        if (n == 1 || m == 1) {
+            return 1;
+        }
+        int[] pre = new int[m];
+        for (int i = 0; i < pre.length; i++) {
+            pre[i] = 1;
+        }
+        return process1(pre, 0, n);
+    }
+
+    /**
+     * 假定level-2行及 以上都摆满了
+     *
+     * @param pre       level-1行的状态
+     * @param level     正在level行做决定
+     * @param n         一共有多少航
+     * @return          返回方法数
+     */
+    public static int process1(int[] pre, int level, int n) {
+        if (level == n) {
+            for (int i = 0; i < pre.length; i++) {
+                if (pre[i] == 0) {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+        // 没到终止行，可以选择在当前的level行摆瓷砖
+        int[] op = getOp(pre);
+        return dfs1(op, 0, level, n);
+    }
+
+    // op[i] == 0 可以考虑摆砖
+    // op[i] == 1 只能竖着向上
+    public static int dfs1(int[] op, int col, int level, int n) {
+        // 在列上自由发挥，深度优先遍历，col来到终止列，i行的决定做完了
+        // 终止列
+        if (col == op.length) {
+            return process1(op, level + 1, n);
+        }
+        int ans = 0;
+        // col位置不横摆
+        ans += dfs1(op, col + 1, level, n);
+        // col位置横摆
+        if (col + 1 < op.length && op[col] == 0 && op[col + 1] == 0) {
+            op[col] = 1;
+            op[col + 1] = 1;
+            ans += dfs1(op, col + 2, level, n);
+            op[col] = 0;
+            op[col + 1] = 0;
+        }
+        return ans;
+    }
+
+    public static int[] getOp(int[] pre) {
+        int[] cur = new int[pre.length];
+        for (int i = 0; i < pre.length; i++) {
+            cur[i] = pre[i] ^ 1;
+        }
+        return cur;
+    }
+
+    public static int ways2(int n, int m) {
+        if (n < 1 || m < 1 || ((n * m) & 1) != 0) {
+            return 0;
+        }
+        if (n == 1 || m == 1) {
+            return 1;
+        }
+        int max = Math.max(n, m);
+        int min = Math.min(n, m);
+        int pre = (1 << min) - 1;
+        return process2(pre, 0, max, min);
+    }
+
+    public static int process2(int pre, int i, int n, int m) {
+        if (i == n) {
+            return pre == ((1 << m) - 1) ? 1 : 0;
+        }
+        int op = ((~pre) & ((1 << m) - 1));
+        return dfs2(op, m - 1, i, n, m);
+    }
+
+    public static int dfs2(int op, int col, int level, int n, int m) {
+        if (col == -1) {
+            return process2(op, level + 1, n, m);
+        }
+        int ans = 0;
+        // col位置不横摆
+        ans += dfs2(op, col - 1, level, n, m);
+        // col位置横摆
+        if ((op & (1 << col)) == 0 && col - 1 >= 0 && (op & (1 << (col - 1))) == 0) {
+            ans += dfs2((op | (3 << (col - 1))), col - 2, level, n, m);
+        }
+        return ans;
+    }
+
+    // 记忆化搜索的解
+    // min(n,m) 不超过32
+    public static int ways3(int n, int m) {
+        if (n < 1 || m < 1 || ((n * m) & 1) != 0) {
+            return 0;
+        }
+        if (n == 1 || m == 1) {
+            return 1;
+        }
+        int max = Math.max(n, m);
+        int min = Math.min(n, m);
+        int pre = (1 << min) - 1;
+        int[][] dp = new int[1 << min][max + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return process3(pre, 0, max, min, dp);
+    }
+
+    public static int process3(int pre, int i, int n, int m, int[][] dp) {
+        if (dp[pre][i] != -1) {
+            return dp[pre][i];
+        }
+        int ans = 0;
+        if (i == n) {
+            ans = pre == ((1 << m) - 1) ? 1 : 0;
+        } else {
+            int op = ((~pre) & ((1 << m) - 1));
+            ans = dfs3(op, m - 1, i, n, m, dp);
+        }
+        dp[pre][i] = ans;
+        return ans;
+    }
+
+    public static int dfs3(int op, int col, int level, int n, int m, int[][] dp) {
+        if (col == -1) {
+            return process3(op, level + 1, n, m, dp);
+        }
+        int ans = 0;
+        ans += dfs3(op, col - 1, level, n, m, dp);
+        if (col > 0 && (op & (3 << (col - 1))) == 0) {
+            ans += dfs3((op | (3 << (col - 1))), col - 2, level, n, m, dp);
+        }
+        return ans;
+    }
+
+    // 严格位置依赖的动态规划
+    public static int ways4(int n, int m) {
+        if (n < 1 || m < 1 || ((n * m) & 1) != 0) {
+            return 0;
+        }
+        if (n == 1 || m == 1) {
+            return 1;
+        }
+        int max = Math.max(n, m);
+        int min = Math.min(n, m);
+        int sn = 1 << min;
+        int limit = sn - 1;
+        int[] dp = new int[sn];
+        dp[limit] = 1;
+        int[] cur = new int[sn];
+        for (int level = 0; level < max; level++) {
+            for (int status = 0; status < sn; status++) {
+                if (dp[status] != 0) {
+                    int op = (~status) & limit;
+                    dfs4(dp[status], op, 0, min - 1, cur);
+                }
+            }
+            for (int i = 0; i < sn; i++) {
+                dp[i] = 0;
+            }
+            int[] tmp = dp;
+            dp = cur;
+            cur = tmp;
+        }
+        return dp[limit];
+    }
+
+    public static void dfs4(int way, int op, int index, int end, int[] cur) {
+        if (index == end) {
+            cur[op] += way;
+        } else {
+            dfs4(way, op, index + 1, end, cur);
+            if (((3 << index) & op) == 0) {
+                dfs4(way, op | (3 << index), index + 1, end, cur);
+            }
+        }
+    }
+}
--- src/main/java/com/serendipity/skills/statecompression/TSP.java
+++ src/main/java/com/serendipity/skills/statecompression/TSP.java
@@ -52,7 +52,11 @@ public class TSP {
     }
 
     public static int min1(int[][] matrix) {
+        // 0...N-1
         int len = matrix.length;
+        // set
+        // set.get(i) != null i这座城市在集合里
+        // set.get(i) == null i这座城市不在集合里
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < len; i++) {
             list.add(1);
@@ -60,8 +64,9 @@ public class TSP {
         return func1(matrix, list, 0);
     }
 
+    // 任何两座城市之间的距离，可以在matrix里面拿到
     // start一定在list中
-    // 从start出发，将list中的城市走一遍，最终回到9这座城市的最小距离
+    // 从start出发，将list中所有的城市走一遍，最终回到0这座城市的最小距离
     public static int func1(int[][] matrix, List<Integer> list, int start) {
         int cityNum = 0;
         for (int i = 0; i < list.size(); i++) {
@@ -72,10 +77,12 @@ public class TSP {
         if (cityNum == 1) {
             return matrix[start][0];
         }
+        // cityNum > 1  不只start这一座城
         list.set(start, null);
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i) != null) {
+                // start -> i i... -> 0
                 int cur = matrix[start][i] + func1(matrix, list, i);
                 min = Math.min(min, cur);
             }
@@ -85,11 +92,17 @@ public class TSP {
     }
 
     public static int min2(int[][] matrix) {
+        // 0...N-1
         int len = matrix.length;
+        // 7座城 1111111
         int allCity = (1 << len) - 1;
         return func2(matrix, allCity, 0);
     }
 
+    // 任何两座城市之间的距离，可以在matrix里面拿到
+    // set中表示着哪些城市的集合，
+    // start这座城一定在set里，
+    // 从start出发，要把set中所有的城市过一遍，最终回到0这座城市，最小距离是多少
     public static int func2(int[][] matrix, int cityStatus, int start) {
         // cityStatus的二进制只有一个1
         if (cityStatus == (cityStatus & (~cityStatus + 1))) {
@@ -98,6 +111,7 @@ public class TSP {
         // 把start位的1去掉
         cityStatus &= (~(1 << start));
         int min = Integer.MAX_VALUE;
+        // 枚举所有的城市
         for (int move = 0; move < matrix.length; move++) {
             if ((cityStatus & (1 << move)) != 0) {
                 int cur = matrix[start][move] + func2(matrix, cityStatus, move);
@@ -110,7 +124,9 @@ public class TSP {
 
     // 先证明存在重复的组合，再使用缓存
     public static int min3(int[][] matrix) {
+        // 0...N-1
         int len = matrix.length;
+        // 7座城 1111111
         int allCity = (1 << len) - 1;
         int[][] dp = new int[1 << len][len];
         for (int i = 0; i < (1 << len); i++) {
@@ -121,6 +137,10 @@ public class TSP {
         return func3(matrix, allCity, 0, dp);
     }
 
+    // 任何两座城市之间的距离，可以在matrix里面拿到
+    // set中表示着哪些城市的集合，
+    // start这座城一定在set里，
+    // 从start出发，要把set中所有的城市过一遍，最终回到0这座城市，最小距离是多少
     public static int func3(int[][] matrix, int cityStatus, int start, int[][] dp) {
         if (dp[cityStatus][start] != -1) {
             return dp[cityStatus][start];
@@ -132,6 +152,7 @@ public class TSP {
             // 把start位的1去掉
             cityStatus &= (~(1 << start));
             int min = Integer.MAX_VALUE;
+            // 枚举所有的城市
             for (int move = 0; move < matrix.length; move++) {
                 if (move != start && (cityStatus & (1 << move)) != 0) {
                     int cur = matrix[start][move] + func3(matrix, cityStatus, move, dp);
@@ -145,43 +166,56 @@ public class TSP {
     }
 
     public static int min4(int[][] matrix) {
+        // 0...N-1
         int len = matrix.length;
         int statusNums = 1 << len;
         int[][] dp = new int[statusNums][len];
         for (int status = 0; status < statusNums; status++) {
             for (int start = 0; start < len; start++) {
-                if (status == (status & (~status + 1))) {
-                    dp[status][start] = matrix[start][0];
-                } else {
-                    // 把start位的1去掉
-                    int min = Integer.MAX_VALUE;
-                    int pre = status & (~(1 << start));
-                    for (int i = 0; i < len; i++) {
-                        if ((pre & (1 << i)) != 0) {
-                            int cur = matrix[start][i] + dp[pre][i];
-                            min = Math.min(min, cur);
+                if ((status & (1 << start)) != 0) {
+                    if (status == (status & (~status + 1))) {
+                        dp[status][start] = matrix[start][0];
+                    } else {
+                        int min = Integer.MAX_VALUE;
+                        // start 城市在status里去掉之后，的状态
+                        int pre = status & (~(1 << start));
+                        // start -> i
+                        for (int i = 0; i < len; i++) {
+                            if ((pre & (1 << i)) != 0) {
+                                int cur = matrix[start][i] + dp[pre][i];
+                                min = Math.min(min, cur);
+                            }
                         }
+                        dp[status][start] = min;
                     }
-                    dp[status][start] = min;
                 }
             }
         }
         return dp[statusNums - 1][0];
     }
 
+    // matrix[i][j] -> i城市到j城市的距离
     public static int tsp1(int[][] matrix, int origin) {
         if (matrix == null || matrix.length < 2 || origin < 0 || origin >= matrix.length) {
             return 0;
         }
+        // 要考虑的集合
         List<Integer> cities = new ArrayList<>();
+        // cities[0] != null 表示0城在集合里
         // cities[i] != null 表示i城在集合里
         for (int i = 0; i < matrix.length; i++) {
             cities.add(1);
         }
+        // null,1,1,1,1,1,1
+        // origin城不参与集合
         cities.set(origin, null);
         return process1(matrix, origin, cities, origin);
     }
 
+    // matrix 所有距离，存在其中
+    // origin 固定参数，唯一的目标
+    // cities 要考虑的集合，一定不含有origin
+    // 当前来到的城市是谁，cur
     public static int process1(int[][] matrix, int aim, List<Integer> cities, int cur) {
         // 集合中是否还有城市
         boolean hasCity = false;
@@ -190,6 +224,7 @@ public class TSP {
             if (cities.get(i) != null) {
                 hasCity = true;
                 cities.set(i, null);
+                // matrix[cur][i] + f(i, 集团(去掉i) )
                 ans = Math.min(ans, matrix[cur][i] + process1(matrix, aim, cities, i));
                 cities.set(i, 1);
             }
@@ -197,6 +232,8 @@ public class TSP {
         return hasCity ? ans : matrix[cur][aim];
     }
 
+    // cities 里，一定含有cur这座城
+    // 解决的是，集合从cur出发，通过集合里所有的城市，最终来到aim，最短距离
     public static int process2(int[][] matrix, int aim, List<Integer> cities, int cur) {
         if (cities.size() == 1) {
             return matrix[cur][aim];
@@ -226,17 +263,22 @@ public class TSP {
         int kcity = 0;
         for (int i = 0; i < len; i++) {
             icity = i < origin ? i : i + 1;
+            // 00000000 i
             dp[0][i] = matrix[icity][origin];
         }
-        for (int status = 0; status < s; status++) {
+        for (int status = 1; status < s; status++) {
             // 尝试每一种状态
             for (int i = 0; i < len; i++) {
+                // i 枚举的出发城市
                 dp[status][i] = Integer.MAX_VALUE;
                 if ((1 << i & status) != 0) {
-                    // 如果这个城市是可以枚举的，i = 6  i对应的原始城市编号icity
+                    // 如果i这座城是可以枚举的，i = 6 ， i对应的原始城的编号，icity
                     icity = i < origin ? i : i + 1;
+                    // i 这一步连到的点，k
                     for (int k = 0; k < len; k++) {
+                        // i 这一步可以连到k
                         if ((1 << k & status) != 0) {
+                            // k对应的原始城的编号，kcity
                             kcity = k < origin ? k : k + 1;
                             dp[status][i] = Math.min(dp[status][i], dp[status ^ (1 << i)][k] + matrix[icity][kcity]);
                         }
