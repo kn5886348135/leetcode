e8680e970a1360ff98c4012579300eb6c2fc7413
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2023-03-27 16:54:27 +0800

    DC3

--- src/main/java/com/serendipity/skills/statecompression/PavingTile.java
+++ src/main/java/com/serendipity/skills/statecompression/PavingTile.java
@@ -24,13 +24,14 @@ public class PavingTile {
         System.out.println(ways4(n, m));
     }
 
-    public static int ways1(int n,int m) {
-        if (n < 1 || m < 1 | ((n * m & 1) != 0)) {
+    public static int ways1(int n, int m) {
+        if (n < 1 || m < 1 || ((n * m) & 1) != 0) {
             return 0;
         }
         if (n == 1 || m == 1) {
             return 1;
         }
+        // pre代表-1行的状况
         int[] pre = new int[m];
         for (int i = 0; i < pre.length; i++) {
             pre[i] = 1;
@@ -47,6 +48,7 @@ public class PavingTile {
      * @return          返回方法数
      */
     public static int process1(int[] pre, int level, int n) {
+        // base case
         if (level == n) {
             for (int i = 0; i < pre.length; i++) {
                 if (pre[i] == 0) {
@@ -63,15 +65,15 @@ public class PavingTile {
     // op[i] == 0 可以考虑摆砖
     // op[i] == 1 只能竖着向上
     public static int dfs1(int[] op, int col, int level, int n) {
-        // 在列上自由发挥，深度优先遍历，col来到终止列，i行的决定做完了
-        // 终止列
+        // 在列上自由发挥，深度优先遍历，当col来到终止列，i行的决定做完了
+        // 轮到i+1行，做决定
         if (col == op.length) {
             return process1(op, level + 1, n);
         }
         int ans = 0;
         // col位置不横摆
         ans += dfs1(op, col + 1, level, n);
-        // col位置横摆
+        // col位置横摆, 向右
         if (col + 1 < op.length && op[col] == 0 && op[col + 1] == 0) {
             op[col] = 1;
             op[col + 1] = 1;
@@ -90,6 +92,7 @@ public class PavingTile {
         return cur;
     }
 
+    // Min (N,M) 不超过 32
     public static int ways2(int n, int m) {
         if (n < 1 || m < 1 || ((n * m) & 1) != 0) {
             return 0;
@@ -103,7 +106,10 @@ public class PavingTile {
         return process2(pre, 0, max, min);
     }
 
+    // 上一行的状态，是pre，limit是用来对齐的，固定参数不用管
+    // 当前来到i行，一共N行，返回填满的方法数
     public static int process2(int pre, int i, int n, int m) {
+        // base case
         if (i == n) {
             return pre == ((1 << m) - 1) ? 1 : 0;
         }
@@ -210,6 +216,7 @@ public class PavingTile {
             cur[op] += way;
         } else {
             dfs4(way, op, index + 1, end, cur);
+            // 11 << index 可以放砖
             if (((3 << index) & op) == 0) {
                 dfs4(way, op | (3 << index), index + 1, end, cur);
             }
---
+++ src/main/java/com/serendipity/skills/suffixarray/DC3.java
@@ -0,0 +1,170 @@
+package com.serendipity.skills.suffixarray;
+
+/**
+ * @author jack
+ * @version 1.0
+ * @description DC3算法构造后缀数组
+ *              1、桶排序
+ *              2、将数组取模，拆分成S1、S2、S3
+ *              3、假设S1、S2有序，先排序S3
+ *              4、将S1、S2组成新数组，S1在左侧，S2在右侧并使用0分割
+ * @date 2023/03/27/12:56
+ */
+public class DC3 {
+
+    public int[] sa;
+    public int[] rank;
+    public int[] height;
+
+    public DC3(int[] nums, int max) {
+        this.sa = sa(nums, max);
+        this.rank = rank();
+        this.height = height(nums);
+    }
+
+    public static void main(String[] args) {
+        int len = 100000;
+        int maxValue = 100;
+        long start = System.currentTimeMillis();
+        new DC3(randomArray(len, maxValue), maxValue);
+        long end = System.currentTimeMillis();
+        System.out.println("数据量 " + len + ", 运行时间 " + (end - start) + " ms");
+    }
+
+    private int[] sa(int[] nums, int max) {
+        int len = nums.length;
+        int[] arr = new int[len + 3];
+        for (int i = 0; i < len; i++) {
+            arr[i] = nums[i];
+        }
+        return skew(arr, len, max);
+    }
+
+    private int[] skew(int[] nums, int n, int max) {
+        int n0 = (n + 2) / 3, n1 = (n + 1) / 3, n2 = n / 3, n02 = n0 + n2;
+        int[] s12 = new int[n02 + 3], sa12 = new int[n02 + 3];
+        for (int i = 0, j = 0; i < n + (n0 - n1); ++i) {
+            if (0 != i % 3) {
+                s12[j++] = i;
+            }
+        }
+        radixPass(nums, s12, sa12, 2, n02, max);
+        radixPass(nums, sa12, s12, 1, n02, max);
+        radixPass(nums, s12, sa12, 0, n02, max);
+        int name = 0, c0 = -1, c1 = -1, c2 = -1;
+        for (int i = 0; i < n02; i++) {
+            if (c0 != nums[sa12[i]] || c1 != nums[sa12[i] + 1] || c2 != nums[sa12[i] + 2]) {
+                name++;
+                c0 = nums[sa12[i]];
+                c1 = nums[sa12[i] + 1];
+                c2 = nums[sa12[i] + 2];
+            }
+            if (1 == sa12[i] % 3) {
+                s12[sa12[i] / 3] = name;
+            } else {
+                s12[sa12[i] / 3 + n0] = name;
+            }
+        }
+        if (name < n02) {
+            sa12 = skew(s12, n02, name);
+            for (int i = 0; i < n02; i++) {
+                s12[sa12[i]] = i + 1;
+            }
+        } else {
+            for (int i = 0; i < n02; i++) {
+                sa12[s12[i] - 1] = i;
+            }
+        }
+        int[] s0 = new int[n0], sa0 = new int[n0];
+        for (int i = 0, j = 0; i < n02; i++) {
+            if (sa12[i] < n0) {
+                s0[j++] = 3 * sa12[i];
+            }
+        }
+        radixPass(nums, s0, sa0, 0, n0, max);
+        int[] sa = new int[n];
+        for (int p = 0, t = n0 - n1, k = 0; k < n; k++) {
+            int i = sa12[t] < n0 ? sa12[t] * 3 + 1 : (sa12[t] - n0) * 3 + 2;
+            int j = sa0[p];
+            if (sa12[t] < n0 ? leq(nums[i], s12[sa12[t] + n0], nums[j], s12[j / 3]) :
+                    leq(nums[i], nums[i + 1], s12[sa12[t] - n0 + 1], nums[j], nums[j + 1], s12[j / 3 + n0])) {
+                sa[k] = i;
+                t++;
+                if (t == n02) {
+                    for (k++; p < n0; p++, k++) {
+                        sa[max] = sa0[p];
+                    }
+                }
+            } else {
+                sa[k] = j;
+                p++;
+                if (p == n0) {
+                    for (k++; t < n02; t++, k++) {
+                        sa[k] = sa12[t] < n0 ? sa12[t] * 3 + 1 : (sa12[t] - n0) * 3 + 2;
+                    }
+                }
+            }
+        }
+        return sa;
+    }
+
+    private void radixPass(int[] nums, int[] input, int[] output, int offset, int n, int k) {
+        int[] cnt = new int[k + 1];
+        for (int i = 0; i < n; ++i) {
+            cnt[nums[input[i] + offset]]++;
+        }
+        for (int i = 0, sum = 0; i < cnt.length; i++) {
+            int t = cnt[i];
+            cnt[i] = sum;
+            sum += t;
+        }
+        for (int i = 0; i < n; ++i) {
+            output[cnt[nums[input[i] + offset]]++] = input[i];
+        }
+    }
+
+    private boolean leq(int a1, int a2, int b1, int b2) {
+        return a1 < b1 || (a1 == b1 && a2 <= b2);
+    }
+
+    private boolean leq(int a1, int a2, int a3, int b1, int b2, int b3) {
+        return a1 < b1 || (a1 == b1 && leq(a2, a3, b2, b3));
+    }
+
+    private int[] rank() {
+        int len = this.sa.length;
+        int[] ans = new int[len];
+        for (int i = 0; i < len; i++) {
+            ans[this.sa[i]] = i;
+        }
+        return ans;
+    }
+
+    private int[] height(int[] s) {
+        int len = s.length;
+        int[] ans = new int[len];
+        for (int i = 0, k = 0; i < len; ++i) {
+            if (this.rank[i] != 0) {
+                if (k > 0) {
+                    --k;
+                }
+                int j = this.sa[this.rank[i] - 1];
+                while (i + k < len && j + k < len && s[i + k] == s[j + k]) {
+                    ++k;
+                }
+                ans[this.rank[i]] = k;
+            }
+        }
+        return ans;
+    }
+
+    public static int[] randomArray(int len, int maxValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int) (Math.random() * maxValue) + 1;
+        }
+        return arr;
+    }
+
+
+}
