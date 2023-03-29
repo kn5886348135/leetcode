43b75bba35a5d793430b469ed3f68715c012a3e4
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2023-03-29 21:54:14 +0800

    自定义堆

---
+++ src/main/java/com/serendipity/heap/Heap.java
@@ -0,0 +1,175 @@
+package com.serendipity.heap;
+
+import com.sun.org.apache.bcel.internal.generic.SWAP;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author jack
+ * @version 1.0
+ * @description 自定义heap的实现
+ * @date 2023/03/29/21:32
+ */
+public class Heap {
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        heap.add(5);
+        heap.add(5);
+        heap.add(5);
+        heap.add(3);
+        // 5 , 3
+        System.out.println(heap.peek());
+        heap.add(7);
+        heap.add(0);
+        heap.add(7);
+        heap.add(0);
+        heap.add(7);
+        heap.add(0);
+        System.out.println(heap.peek());
+        while (!heap.isEmpty()) {
+            System.out.println(heap.poll());
+        }
+
+        int value = 1000;
+        int limit = 100;
+        int testTimes = 1000000;
+        for (int i = 0; i < testTimes; i++) {
+            int curLimit = (int) (Math.random() * limit) + 1;
+            CustomMaxHeap my = new CustomMaxHeap(curLimit);
+            RightMaxHeap test = new RightMaxHeap(curLimit);
+            int curOpTimes = (int) (Math.random() * limit);
+            for (int j = 0; j < curOpTimes; j++) {
+                if (my.isEmpty() != test.isEmpty()) {
+                    System.out.println("Oops!");
+                }
+                if (my.isFull() != test.isFull()) {
+                    System.out.println("Oops!");
+                }
+                if (my.isEmpty()) {
+                    int curValue = (int) (Math.random() * value);
+                    my.push(curValue);
+                    test.push(curValue);
+                } else if (my.isFull()) {
+                    if (my.pop() != test.pop()) {
+                        System.out.println("Oops!");
+                    }
+                } else {
+                    if (Math.random() < 0.5) {
+                        int curValue = (int) (Math.random() * value);
+                        my.push(curValue);
+                        test.push(curValue);
+                    } else {
+                        if (my.pop() != test.pop()) {
+                            System.out.println("Oops!");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static class CustomMaxHeap {
+        private int[] heap;
+        private final int limit;
+        private int heapSize;
+
+        public CustomMaxHeap(int limit) {
+            this.heap = new int[limit];
+            this.limit = limit;
+            this.heapSize = 0;
+        }
+
+        public boolean isEmpty() {
+            return heapSize == 0;
+        }
+
+        public boolean isFull() {
+            return heapSize == limit;
+        }
+
+        public void push(int value) {
+            if (heapSize == limit) {
+                throw new RuntimeException("heap is full");
+            }
+            heap[heapSize++] = value;
+            // value heapSize
+            heapInsert(heap, heapSize++);
+        }
+
+        public int pop() {
+            int ans = heap[0];
+            swap(heap, 0, --heapSize);
+            heapify(heap, 0, heapSize);
+            return ans;
+        }
+
+        private void heapInsert(int[] arr, int index) {
+            while (arr[index] > arr[(index - 1) / 2]) {
+                swap(arr, index, (index - 1) / 2);
+                index = (index - 1) / 2;
+            }
+        }
+
+        private void heapify(int[] arr, int index, int heapSize) {
+            int left = index * 2 + 1;
+            // 如果有左子节点，可能有右子节点也可能没有
+            while (left < heapSize) {
+                int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+                largest = arr[largest] > arr[index] ? largest : index;
+                if (largest == index) {
+                    break;
+                }
+                swap(arr, largest, index);
+                index = largest;
+                left = index * 2 + 1;
+            }
+        }
+
+        private void swap(int[] arr, int i, int j) {
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+
+    public static class RightMaxHeap {
+        private int[] arr;
+        private final int limit;
+        private int size;
+
+        public RightMaxHeap(int limit) {
+            this.arr = new int[limit];
+            this.limit = limit;
+            this.size = 0;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public boolean isFull() {
+            return size == limit;
+        }
+
+        public void push(int value) {
+            if (size == limit) {
+                throw new RuntimeException("heap is full");
+            }
+            arr[size++] = value;
+        }
+
+        public int pop() {
+            int maxIndex = 0;
+            for (int i = 1; i < size; i++) {
+                if (arr[i] > arr[maxIndex]) {
+                    maxIndex = i;
+                }
+            }
+            int ans = arr[maxIndex];
+            arr[maxIndex] = arr[--size];
+            return ans;
+        }
+    }
+}
--- src/main/java/com/serendipity/sort/HeapSort.java
+++ src/main/java/com/serendipity/sort/HeapSort.java
@@ -46,6 +46,7 @@ public class HeapSort {
         printArray(arr);
     }
 
+    // 堆排序额外空间复杂度O(1)
     public static void heapSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
@@ -56,7 +57,7 @@ public class HeapSort {
 //            heapInsert(arr, i);
 //        }
         // O(N)
-        for (int i = arr.length - 1; i >= 0; i++) {
+        for (int i = arr.length - 1; i >= 0; i--) {
             heapify(arr, i, arr.length);
         }
         int heapSize = arr.length;
@@ -70,8 +71,9 @@ public class HeapSort {
         }
     }
 
+    // arr[index]刚来的数，往上
     public static void heapInsert(int[] arr, int index) {
-        while (arr[index] > arr[index - 1] / 2) {
+        while (arr[index] > arr[(index - 1) / 2]) {
             swap(arr, index, (index - 1) / 2);
             index = (index - 1) / 2;
         }
@@ -82,8 +84,14 @@ public class HeapSort {
         // 左边子节点的下标
         int left = index * 2 + 1;
         while (left < heapSize) {
+            // 下方还有孩子的时候
+            // 两个孩子中，谁的值大，把下标给largest
+            // 1）只有左孩子，left -> largest
+            // 2) 同时有左孩子和右孩子，右孩子的值<= 左孩子的值，left -> largest
+            // 3) 同时有左孩子和右孩子并且右孩子的值> 左孩子的值， right -> largest
             int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
-            largest = arr[largest] > arr[largest] ? largest : index;
+            // 父和较大的孩子之间，谁的值大，把下标给largest
+            largest = arr[largest] > arr[index] ? largest : index;
             if (largest == index) {
                 break;
             }
