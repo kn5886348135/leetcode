cde51b99bf40b6fe949fb0c31b30e2735f2282bb
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2023-03-30 15:23:23 +0800

    翻转对

---
+++ src/main/java/com/serendipity/sort/LeetCode493.java
@@ -0,0 +1,122 @@
+package com.serendipity.sort;
+
+/**
+ * @author jack
+ * @version 1.0
+ * @description 给定一个数组 nums ，如果 i < j 且 nums[i] > 2*nums[j] 我们就将 (i, j) 称作一个重要翻转对。
+ *              你需要返回给定数组中的重要翻转对的数量。
+ * @date 2023/03/30/15:21
+ */
+public class LeetCode493 {
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = new int[arr1.length];
+            System.arraycopy(arr1, 0, arr2, 0, arr1.length);
+            if (reversePairs(arr1) != comparator(arr2)) {
+                System.out.println("Oops!");
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+    }
+
+    public static int reversePairs(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        return process(arr, 0, arr.length - 1);
+    }
+
+    public static int process(int[] arr, int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        // l < r
+        int mid = l + ((r - l) >> 1);
+        return process(arr, l, mid) + process(arr, mid + 1, r) + merge(arr, l, mid, r);
+    }
+
+    public static int merge(int[] arr, int L, int m, int r) {
+        // [L....M] [M+1....R]
+        int ans = 0;
+        // 目前囊括进来的数，是从[M+1, windowR)
+        int windowR = m + 1;
+        for (int i = L; i <= m; i++) {
+            while (windowR <= r && (long) arr[i] > (long) arr[windowR] * 2) {
+                windowR++;
+            }
+            ans += windowR - m - 1;
+        }
+        int[] help = new int[r - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = m + 1;
+        while (p1 <= m && p2 <= r) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[L + i] = help[i];
+        }
+        return ans;
+    }
+
+    public static int comparator(int[] arr) {
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > (arr[j] << 1)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) ((maxValue + 1) * Math.random());
+        }
+        return arr;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
