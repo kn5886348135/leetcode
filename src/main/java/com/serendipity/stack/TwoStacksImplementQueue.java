51709f50d17adc8d2ad1f09e1eb9299dc6babfff
kn5886348135 <56352848+kn5886348135@users.noreply.github.com>
2023-03-30 16:00:10 +0800

    队列实现栈，栈实现队列

---
+++ src/main/java/com/serendipity/stack/TwoStacksImplementQueue.java
@@ -0,0 +1,65 @@
+package com.serendipity.stack;
+
+import java.util.Stack;
+
+/**
+ * @author jack
+ * @version 1.0
+ * @description 两个栈实现队列
+ * @date 2023/03/30/15:55
+ */
+public class TwoStacksImplementQueue {
+
+    public static void main(String[] args) {
+        TwoStacksQueue test = new TwoStacksQueue();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        System.out.println(test.peek());
+        System.out.println(test.poll());
+        System.out.println(test.peek());
+        System.out.println(test.poll());
+        System.out.println(test.peek());
+        System.out.println(test.poll());
+    }
+
+    public static class TwoStacksQueue {
+        public Stack<Integer> stackPush;
+        public Stack<Integer> stackPop;
+
+        public TwoStacksQueue() {
+            stackPush = new Stack<>();
+            stackPop = new Stack<>();
+        }
+
+        // push栈向pop栈倒入数据
+        private void pushToPop() {
+            if (stackPop.empty()) {
+                while (!stackPush.empty()) {
+                    stackPop.push(stackPush.pop());
+                }
+            }
+        }
+
+        public void add(int pushInt) {
+            stackPush.push(pushInt);
+            pushToPop();
+        }
+
+        public int poll() {
+            if (stackPop.empty() && stackPush.empty()) {
+                throw new RuntimeException("Queue is empty!");
+            }
+            pushToPop();
+            return stackPop.pop();
+        }
+
+        public int peek() {
+            if (stackPop.empty() && stackPush.empty()) {
+                throw new RuntimeException("Queue is empty!");
+            }
+            pushToPop();
+            return stackPop.peek();
+        }
+    }
+}
