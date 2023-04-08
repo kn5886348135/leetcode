29ea2e5ca5278383c4de78ac7f97cedd2d64c2e5
kn5886348135 <kn5886348135@163.com>
2023-04-08 17:49:04 +0800

    复制带随机指针的链表

---
+++ src/main/java/com/serendipity/algo3linkedlist/LeetCode138.java
@@ -0,0 +1,97 @@
+package com.serendipity.algo3linkedlist;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jack
+ * @version 1.0
+ * @description 复制带随机指针的链表
+ *              给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+ *              构造这个链表的 深拷贝。 深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。新节点的
+ *              next 指针和 random指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。复制
+ *              链表中的指针都不应指向原链表中的节点 。
+ *              例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有
+ *              x.random --> y 。返回复制链表的头节点。
+ *
+ *              用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+ *              val：一个表示 Node.val 的整数。
+ *              random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+ *              你的代码 只 接受原链表的头节点 head 作为传入参数。
+ * @date 2023/04/08/17:07
+ */
+public class LeetCode138 {
+
+    // map保存新链表和原链表的对应关系
+    public Node copyRandomList1(Node head) {
+        if (head == null) {
+            return null;
+        }
+        // 保存原链表和新链表节点的对应关系
+        Node cur = head;
+        Map<Node, Node> map = new HashMap<>();
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        // 设置新链表的next和random
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+
+    // 新链表复制在原链表中，然后拆开
+    // A -> B -> C...   A -> a -> B -> b -> C -> c...
+    public Node copyRandomList2(Node head) {
+        if (head == null) {
+            return null;
+        }
+        // 复制原链表节点并拼接在对应节点后面
+        Node cur = head;
+        Node next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = new Node(cur.val);
+            cur.next.next = next;
+            cur = next;
+        }
+        // 设置新链表的random
+        cur = head;
+        Node copy = null;
+        copy = cur.next;
+        Node copyCur = copy;
+        while (cur != null) {
+            next = cur.next.next;
+            copy = cur.next;
+            copyCur.random = cur.random != null ? cur.random.next : null;
+            cur = next;
+        }
+        // 将新链表从原链表中拆出来
+        Node res = head.next;
+        cur = head;
+        while (cur != null) {
+            next = cur.next.next;
+            copy = cur.next;
+            cur.next = next;
+            copy.next = next != null ? next.next : null;
+            cur = next;
+        }
+        return res;
+    }
+
+    public class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
