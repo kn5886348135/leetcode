72b6079b66e8feebedf95072dbb9dfd3ae4633bb
kn5886348135 <kn5886348135@163.com>
2023-04-09 22:18:48 +0800

    逆序对

--- src/main/java/com/serendipity/algo4sort/ReversePair.java
+++ src/main/java/com/serendipity/algo4sort/ReversePair.java
@@ -1,5 +1,7 @@
 package com.serendipity.algo4sort;
 
+import com.serendipity.common.CommonUtil;
+
 /**
  * @author jack
  * @version 1.0
@@ -7,6 +9,7 @@ package com.serendipity.algo4sort;
  *              任何一个前面的数a，和任何一个后面的数b，
  *              如果(a,b)是降序的，就称为逆序对
  *              返回数组中所有的逆序对
+ *              a，b不一定是相邻的
  * @date 2023/03/30/15:17
  */
 public class ReversePair {
@@ -15,19 +18,23 @@ public class ReversePair {
         int testTime = 500000;
         int maxSize = 100;
         int maxValue = 100;
+        boolean success = true;
         for (int i = 0; i < testTime; i++) {
-            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr1 = CommonUtil.generateRandomArray(maxSize, maxValue);
             int[] arr2 = new int[arr1.length];
             System.arraycopy(arr1, 0, arr2, 0, arr1.length);
-            if (reverPairNumber(arr1) != comparator(arr2)) {
+            if (reverPairNumber(arr1) != verifyReversePair(arr2)) {
                 System.out.println("Oops!");
-                printArray(arr1);
-                printArray(arr2);
+                CommonUtil.printArray(arr1);
+                CommonUtil.printArray(arr2);
                 break;
             }
         }
+        System.out.println(success ? "success" : "failed");
     }
 
+    // 逆序对
+    // 归并法
     public static int reverPairNumber(int[] arr) {
         if (arr == null || arr.length < 2) {
             return 0;
@@ -39,38 +46,38 @@ public class ReversePair {
     // 所有merge时，产生的逆序对数量，累加，返回
     // 左 排序 merge并产生逆序对数量
     // 右 排序 merge并产生逆序对数量
-    public static int process(int[] arr, int l, int r) {
-        if (l == r) {
+    public static int process(int[] arr, int left, int right) {
+        if (left == right) {
             return 0;
         }
-        // l < r
-        int mid = l + ((r - l) >> 1);
-        return process(arr, l, mid) + process(arr, mid + 1, r) + merge(arr, l, mid, r);
+        int middle = left + ((right - left) >> 1);
+        return process(arr, left, middle) + process(arr, middle + 1, right) + merge(arr, left, middle, right);
     }
 
-    public static int merge(int[] arr, int L, int m, int r) {
-        int[] help = new int[r - L + 1];
-        int i = help.length - 1;
-        int p1 = m;
-        int p2 = r;
-        int res = 0;
-        while (p1 >= L && p2 > m) {
-            res += arr[p1] > arr[p2] ? (p2 - m) : 0;
-            help[i--] = arr[p1] > arr[p2] ? arr[p1--] : arr[p2--];
+    // 逆序排序
+    public static int merge(int[] arr, int left, int middle, int right) {
+        int[] help = new int[right - left + 1];
+        int index = help.length - 1;
+        int p = middle;
+        int q = right;
+        int ans = 0;
+        while (p >= left && q > middle) {
+            // p遍历左边所有元素，q到m的所有元素都小于arr[p1]
+            ans += arr[p] > arr[q] ? (q - middle) : 0;
+            help[index--] = arr[p] > arr[q] ? arr[p--] : arr[q--];
         }
-        while (p1 >= L) {
-            help[i--] = arr[p1--];
+        while (p >= left) {
+            help[index--] = arr[p--];
         }
-        while (p2 > m) {
-            help[i--] = arr[p2--];
+        while (q > middle) {
+            help[index--] = arr[q--];
         }
-        for (i = 0; i < help.length; i++) {
-            arr[L + i] = help[i];
-        }
-        return res;
+        System.arraycopy(help, 0, arr, left, help.length);
+        return ans;
     }
 
-    public static int comparator(int[] arr) {
+    // 对数器
+    public static int verifyReversePair(int[] arr) {
         int ans = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
@@ -80,41 +87,5 @@ public class ReversePair {
             }
         }
         return ans;
-    }
-
-    public static int[] generateRandomArray(int maxSize, int maxValue) {
-        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
-        }
-        return arr;
-    }
-
-    public static boolean isEqual(int[] arr1, int[] arr2) {
-        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
-            return false;
-        }
-        if (arr1 == null && arr2 == null) {
-            return true;
-        }
-        if (arr1.length != arr2.length) {
-            return false;
-        }
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] != arr2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void printArray(int[] arr) {
-        if (arr == null) {
-            return;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
     }
 }
