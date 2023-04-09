5d4ce2576177bef605c8f7538990ef28af036399
kn5886348135 <kn5886348135@163.com>
2023-04-10 05:45:25 +0800

    堆排序

--- src/main/java/com/serendipity/algo4sort/Sort7HeapSort.java
+++ src/main/java/com/serendipity/algo4sort/Sort7HeapSort.java
@@ -1,7 +1,8 @@
 package com.serendipity.algo4sort;
 
+import com.serendipity.common.CommonUtil;
+
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
 /**
  * @author jack
@@ -9,41 +10,34 @@ import java.util.PriorityQueue;
  * @description 堆排序
  * @date 2023/03/29/21:14
  */
-public class HeapSort {
+public class Sort7HeapSort {
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        heap.add(6);
-        heap.add(8);
-        heap.add(0);
-        heap.add(2);
-        heap.add(9);
-        heap.add(1);
-
-        while (!heap.isEmpty()) {
-            System.out.println(heap.poll());
-        }
-
         int testTime = 500000;
         int maxSize = 100;
         int maxValue = 100;
         boolean succeed = true;
         for (int i = 0; i < testTime; i++) {
-            int[] arr1 = generateRandomArray(maxSize, maxValue);
-            int[] arr2 = new int[arr1.length];
-            System.arraycopy(arr1, 0, arr2, 0, arr1.length);
+            int[] arr = CommonUtil.generateRandomArray(maxSize, maxValue);
+            int[] arr1 = new int[arr.length];
+            System.arraycopy(arr, 0, arr1, 0, arr.length);
+            int[] arr2 = new int[arr.length];
+            System.arraycopy(arr, 0, arr2, 0, arr.length);
             heapSort(arr1);
             Arrays.sort(arr2);
-            if (!isEqual(arr1, arr2)) {
+            if (!CommonUtil.isEqual(arr1, arr2)) {
+                System.out.println("heapSort failed");
+                System.out.println("original");
+                CommonUtil.printArray(arr);
+                System.out.println("heapSort result");
+                CommonUtil.printArray(arr1);
+                System.out.println("Arrays.sort result");
+                CommonUtil.printArray(arr2);
                 succeed = false;
                 break;
             }
         }
         System.out.println(succeed ? "success" : "failed");
-        int[] arr = generateRandomArray(maxSize, maxValue);
-        printArray(arr);
-        heapSort(arr);
-        printArray(arr);
     }
 
     // 堆排序额外空间复杂度O(1)
@@ -61,20 +55,20 @@ public class HeapSort {
             heapify(arr, i, arr.length);
         }
         int heapSize = arr.length;
-        swap(arr, 0, --heapSize);
+        CommonUtil.swap(arr, 0, --heapSize);
         // O(N * logN)
         // while循环的时间复杂度 O(N)
         while (heapSize > 0) {
             // O(logN))
             heapify(arr, 0, heapSize);
-            swap(arr, 0, --heapSize);
+            CommonUtil.swap(arr, 0, --heapSize);
         }
     }
 
     // arr[index]刚来的数，往上
     public static void heapInsert(int[] arr, int index) {
         while (arr[index] > arr[(index - 1) / 2]) {
-            swap(arr, index, (index - 1) / 2);
+            CommonUtil.swap(arr, index, (index - 1) / 2);
             index = (index - 1) / 2;
         }
     }
@@ -95,55 +89,9 @@ public class HeapSort {
             if (largest == index) {
                 break;
             }
-            swap(arr, largest, index);
+            CommonUtil.swap(arr, largest, index);
             index = largest;
             left = index * 2 + 1;
         }
-    }
-
-    public static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
-
-    public static void comparator(int[] arr) {
-        Arrays.sort(arr);
-    }
-
-    public static int[] generateRandomArray(int maxSize, int maxValue) {
-        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
-        }
-        return arr;
-    }
-
-    public static boolean isEqual(int[] arr1, int[] arr2) {
-        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
-            return false;
-        }
-        if (arr1 == null && arr2 == null) {
-            return true;
-        }
-        if (arr1.length != arr2.length) {
-            return false;
-        }
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] != arr2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void printArray(int[] arr) {
-        if (arr == null) {
-            return;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
     }
 }
