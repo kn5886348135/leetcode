a7bace1eb03cc31dde798da690f85bf46a531c46
kn5886348135 <kn5886348135@163.com>
2023-04-11 02:55:41 +0800

    基数排序

--- src/main/java/com/serendipity/algo4sort/Sort9RedixSort.java
+++ src/main/java/com/serendipity/algo4sort/Sort9RedixSort.java
@@ -1,36 +1,40 @@
 package com.serendipity.algo4sort;
 
+import com.serendipity.common.CommonUtil;
+
 import java.util.Arrays;
 
 public class Sort9RedixSort {
 
     public static void main(String[] args) {
-        int testTime = 500000;
         int maxSize = 100;
         int maxValue = 100000;
-        boolean succeed = true;
-        for (int i = 0; i < testTime; i++) {
-            int[] arr1 = generateRandomArray(maxSize, maxValue);
-            int[] arr2 = new int[arr1.length];
-            System.arraycopy(arr1, 0, arr2, 0, arr1.length);
+        int testTimes = 500000;
+        boolean success = true;
+        for (int i = 0; i < testTimes; i++) {
+            int[] arr = CommonUtil.generateRandomArray(maxSize, maxValue, true);
+            int[] arr1 = new int[arr.length];
+            System.arraycopy(arr, 0, arr1, 0, arr.length);
+            int[] arr2 = new int[arr.length];
+            System.arraycopy(arr, 0, arr2, 0, arr.length);
             radixSort(arr1);
             Arrays.sort(arr2);
-            if (!isEqual(arr1, arr2)) {
-                succeed = false;
-                printArray(arr1);
-                printArray(arr2);
+            if (!CommonUtil.isEqual(arr1, arr2)) {
+                System.out.println("radixSort failed");
+                System.out.println("original");
+                CommonUtil.printArray(arr);
+                System.out.println("radixSort result");
+                CommonUtil.printArray(arr1);
+                System.out.println("Arrays.sort result");
+                CommonUtil.printArray(arr2);
+                success = false;
                 break;
             }
         }
-        System.out.println(succeed ? "success" : "failed");
-
-        int[] arr = generateRandomArray(maxSize, maxValue);
-        printArray(arr);
-        radixSort(arr);
-        printArray(arr);
+        System.out.println(success ? "success" : "failed");
     }
 
-    // only for no-negative value
+    // 基数排序，只适用于非负、10进制数
     public static void radixSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
@@ -38,6 +42,7 @@ public class Sort9RedixSort {
         radixSort(arr, 0, arr.length - 1, maxbits(arr));
     }
 
+    // 最大值的位数，决定循环次数
     public static int maxbits(int[] arr) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
@@ -51,33 +56,36 @@ public class Sort9RedixSort {
         return res;
     }
 
-    // arr[L..R]排序  ,  最大值的十进制位数digit
+    // 根据arr最大值的十进制位数digit，从右往左遍历
+    // 进桶后count[i]表示某一个digit位置的数字i出现的次数
+    // 计算count[i]的前缀和数组并且覆盖count[i]
+    // 出桶，count[i]表示某一个digit位置的i数字最后一个位置
+    // 辅助数组help覆盖arr数组
+    // 遍历下一个digit，重复以上步骤
     public static void radixSort(int[] arr, int left, int right, int digit) {
+        // 10进制
         final int radix = 10;
-        int i = 0, j = 0;
-        // 有多少个数准备多少个辅助空间
+        int i, j;
+        // 辅助数组
         int[] help = new int[right - left + 1];
-        // 有多少位就进出几次
+        // arr最大数有digit位，根据digit从右往左循环
         for (int d = 1; d <= digit; d++) {
-            // 10个空间
-            // count[0] 当前位(d位)是0的数字有多少个
-            // count[1] 当前位(d位)是(0和1)的数字有多少个
-            // count[2] 当前位(d位)是(0、1和2)的数字有多少个
-            // count[i] 当前位(d位)是(0~i)的数字有多少个
-            // count[0..9]
+            // 桶
             int[] count = new int[radix];
+            // d位置的数字进桶
             for (i = left; i <= right; i++) {
-                // 103  1   3
-                // 209  1   9
                 j = getDigit(arr[i], d);
                 count[j]++;
             }
+            // 计算前缀和，确定d位置的i数字右边界
             for (i = 1; i < radix; i++) {
                 count[i] = count[i] + count[i - 1];
             }
+            // 出桶，根据count数组确定每一个数的位置
             for (i = right; i >= left; i--) {
                 j = getDigit(arr[i], d);
                 help[count[j] - 1] = arr[i];
+                // count[j]自减，有些位置不会到0，但是arr[i]的d位置数字个数是确定的
                 count[j]--;
             }
             for (i = left, j = 0; i <= right; i++, j++) {
@@ -86,43 +94,8 @@ public class Sort9RedixSort {
         }
     }
 
+    // 获取从右往左第d位的数字
     public static int getDigit(int x, int d) {
         return ((x / ((int) Math.pow(10, d - 1))) % 10);
-    }
-
-    public static int[] generateRandomArray(int maxSize, int maxValue) {
-        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) ((maxValue + 1) * Math.random());
-        }
-        return arr;
-    }
-
-    public static boolean isEqual(int[] arr1, int[] arr2) {
-        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
-            return false;
-        }
-        if (arr1 == null && arr2 == null) {
-            return true;
-        }
-        if (arr1.length != arr2.length) {
-            return false;
-        }
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] != arr2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void printArray(int[] arr) {
-        if (arr == null) {
-            return;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
     }
 }
