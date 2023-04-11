30b747858169283ec155463abaaa0c64816f4e18
kn5886348135 <kn5886348135@163.com>
2023-04-11 20:49:47 +0800

    二叉树非递归遍历

--- src/main/java/com/serendipity/algo7binarytree/BinaryTree.java
+++ src/main/java/com/serendipity/algo7binarytree/BinaryTree.java
@@ -1,7 +1,8 @@
 package com.serendipity.algo7binarytree;
 
 import com.serendipity.common.BinaryNode;
-import com.serendipity.common.CommonUtil;
+
+import java.util.Stack;
 
 /**
  * @author jack
@@ -13,22 +14,12 @@ public class BinaryTree {
 
     public static void main(String[] args) {
         BinaryNode root = null;
-        preorderTraversal(root);
+        preOrderTraversal(root);
         System.out.println("=======================");
-        inorderTraversal(root);
+        inOrderTraversal(root);
         System.out.println("=======================");
         postOrderTraversal(root);
         System.out.println("=======================");
-    }
-
-    public static class Node {
-        public int val;
-        public Node left;
-        public Node right;
-
-        public Node(int val) {
-            this.val = val;
-        }
     }
 
     // 先序打印所有节点，前序遍历      头左右
@@ -48,27 +39,27 @@ public class BinaryTree {
         // System.out.print(head.val + "\t");
     }
 
-    // 先序遍历
-    public static void preorderTraversal(BinaryNode head) {
+    // 递归先序遍历
+    public static void preOrderTraversal(BinaryNode head) {
         if (head == null) {
             return;
         }
         System.out.print(head.value + "\t");
-        preorderTraversal(head.left);
-        preorderTraversal(head.right);
+        preOrderTraversal(head.left);
+        preOrderTraversal(head.right);
     }
 
-    // 中序遍历
-    public static void inorderTraversal(BinaryNode head) {
+    // 递归中序遍历
+    public static void inOrderTraversal(BinaryNode head) {
         if (head == null) {
             return;
         }
-        inorderTraversal(head.left);
+        inOrderTraversal(head.left);
         System.out.print(head.value + "\t");
-        inorderTraversal(head.right);
+        inOrderTraversal(head.right);
     }
 
-    // 后序遍历
+    // 递归后序遍历
     public static void postOrderTraversal(BinaryNode head) {
         if (head == null) {
             return;
@@ -76,5 +67,90 @@ public class BinaryTree {
         postOrderTraversal(head.left);
         postOrderTraversal(head.right);
         System.out.print(head.value + "\t");
+    }
+
+    // 非递归先序遍历
+    public static void preOrderTraversalUnRecursive(BinaryNode root) {
+        if (root != null) {
+            Stack<BinaryNode> stack1 = new Stack<>();
+            Stack<BinaryNode> stack2 = new Stack<>();
+            stack1.push(root);
+            while (!stack1.isEmpty()) {
+                // 头 右 左
+                BinaryNode node = stack1.pop();
+                stack2.push(node);
+                if (node.left != null) {
+                    stack1.push(node.left);
+                }
+                // left后入栈，先遍历
+                if (node.right != null) {
+                    stack1.push(node.right);
+                }
+            }
+            // 左 右 头
+            while (!stack2.isEmpty()) {
+                System.out.print(stack2.pop() + "\t");
+            }
+        }
+    }
+
+    // 非递归中序遍历
+    public static void inOrderTraversalUnRecursive(BinaryNode root) {
+        if (root != null) {
+            Stack<BinaryNode> stack = new Stack<>();
+            stack.push(root);
+            while (!stack.isEmpty()) {
+                BinaryNode node = stack.pop();
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
+                System.out.print(node.value + "\t");
+                // left后入栈，先遍历
+                if (node.left != null) {
+                    stack.push(node.left);
+                }
+                stack.push(node.right);
+            }
+        }
+    }
+
+    // 两个栈非递归后序遍历
+    public static void postOrderTraversalUnRecursive1(BinaryNode root) {
+        if (root != null) {
+            Stack<BinaryNode> stack = new Stack<>();
+            stack.push(root);
+            while (!stack.isEmpty()) {
+                BinaryNode node = stack.pop();
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
+                // left后入栈，先遍历
+                if (node.left != null) {
+                    stack.push(node.left);
+                }
+                stack.push(node.right);
+                System.out.print(node.value + "\t");
+            }
+        }
+    }
+
+    // 一个栈非递归后序遍历
+    public static void postOrderTraversalUnRecursive2(BinaryNode root) {
+        if (root != null) {
+            Stack<BinaryNode> stack = new Stack<>();
+            stack.push(root);
+            BinaryNode node = null;
+            while (!stack.isEmpty()) {
+                node = stack.pop();
+                if (node.left != null && root != node.left && root != node.right) {
+                    stack.push(node.left);
+                } else if (node.right != null && root != node.right) {
+                    stack.push(node.right);
+                } else {
+                    System.out.print(node.value + "\t");
+                    root = node;
+                }
+            }
+        }
     }
 }
