5d0eb6508d9079aec0044cdaf9c3fc09a8e5c976
kn5886348135 <kn5886348135@163.com>
2023-04-12 00:45:41 +0800

    后继节点

--- src/main/java/com/serendipity/algo7binarytree/SuccessorNode.java
+++ src/main/java/com/serendipity/algo7binarytree/SuccessorNode.java
@@ -11,7 +11,48 @@ package com.serendipity.algo7binarytree;
 public class SuccessorNode {
 
     public static void main(String[] args) {
-        // TODO 对数器
+        // TODO对数器
+        Node head = new Node(6);
+        head.parent = null;
+        head.left = new Node(3);
+        head.left.parent = head;
+        head.left.left = new Node(1);
+        head.left.left.parent = head.left;
+        head.left.left.right = new Node(2);
+        head.left.left.right.parent = head.left.left;
+        head.left.right = new Node(4);
+        head.left.right.parent = head.left;
+        head.left.right.right = new Node(5);
+        head.left.right.right.parent = head.left.right;
+        head.right = new Node(9);
+        head.right.parent = head;
+        head.right.left = new Node(8);
+        head.right.left.parent = head.right;
+        head.right.left.left = new Node(7);
+        head.right.left.left.parent = head.right.left;
+        head.right.right = new Node(10);
+        head.right.right.parent = head.right;
+
+        Node test = head.left.left;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.left.left.right;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.left;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.left.right;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.left.right.right;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.right.left.left;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.right.left;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.right;
+        System.out.println(test.value + " next: " + getSuccessorNode(test).value);
+        test = head.right.right; // 10's next is null
+        System.out.println(test.value + " next: " + getSuccessorNode(test));
     }
 
     public static class Node {
@@ -28,14 +69,15 @@ public class SuccessorNode {
     // 获取后继节点
     public static Node getSuccessorNode(Node node) {
         if (node == null) {
-            return null;
+            return node;
         }
         // 右节点不为空，返回最左节点
         if (node.right != null) {
             return getLeftMost(node.right);
         } else {
-            // 右节点为空，返回父节点
+            // 右节点为空，后继节点只可能在父节点中
             Node parent = node.parent;
+            // 当前节点必须是其父亲节点左子节点，否则向上寻找
             while (parent != null && parent.right == node) {
                 node = parent;
                 parent = node.parent;
