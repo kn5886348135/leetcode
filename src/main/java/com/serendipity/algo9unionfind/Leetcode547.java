af8516dd777a7d0b511a59692609600cc1b5d5ff
kn5886348135 <kn5886348135@163.com>
2023-07-02 23:47:39 +0800

    省份数量

--- src/main/java/com/serendipity/algo9unionfind/Leetcode547.java
+++ src/main/java/com/serendipity/algo9unionfind/Leetcode547.java
@@ -1,19 +1,30 @@
 package com.serendipity.algo9unionfind;
 
 /**
- * 有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
- * 省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。
- * 给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 isConnected[i][j] = 0 表示二者不直接相连。
- * 返回矩阵中 省份 的数量。
- *
  * @author jack
  * @version 1.0
- * @description
+ * @description 省份数量
+ *              有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
+ *
+ *              省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。
+ *
+ *              给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 isConnected[i][j] = 0 表示二者不直接相连。
+ *
+ *              返回矩阵中 省份 的数量。
  * @date 2022/12/16/11:34
  */
 public class Leetcode547 {
+
     public static void main(String[] args) {
         // TODO 对数器
+    }
+
+    public int findCircleNumBFS(int[][] isConnected) {
+        return 0;
+    }
+
+    public int findCircleNumDFS(int[][] isConnected) {
+        return 0;
     }
 
     public static int findCircleNum(int[][] isConnected) {
